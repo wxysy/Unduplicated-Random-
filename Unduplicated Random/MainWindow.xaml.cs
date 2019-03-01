fa37e906c52ef6f857f37ae5b874fe053fe8ecd7
wxysy <41404936+wxysy@users.noreply.github.com>
2019-03-01 10:30:27 +0800

    改进算法中的无用部分，使算法更简洁。

--- Unduplicated Random/MainWindow.xaml.cs
+++ Unduplicated Random/MainWindow.xaml.cs
@@ -36,7 +36,7 @@ namespace Unduplicated_Random
             upper = Convert.ToInt16(parameter_Upper.Text);
             lower = Convert.ToInt16(parameter_Lower.Text);
             number_need = Convert.ToInt16(parameter_Number.Text);
-            number_total = upper - lower + 1;//例如：10-15 一共是10 11 12 13 14 15六个数字
+            number_total = upper - lower;//例如：10-15，10 11 12 13 14 不包括上限15，包括下限10。
 
             //生成索引
             int[] random_index = new int[number_total];
@@ -46,9 +46,6 @@ namespace Unduplicated_Random
                 random_index_result = lower + i;
                 random_index[i] = random_index_result;
             }
-
-            //保存结果用
-            int[] random_value = new int[number_need];
             
             //随机取索引号
             Random random = new Random();
@@ -57,18 +54,16 @@ namespace Unduplicated_Random
             for (int j = 1; j <= number_need; j++)
             {
                 //随机生成一个索引位置
-                random_index_choose = random.Next(0, number_total - 1);
+                random_index_choose = random.Next(0, number_total);//包括最小值索引0，不包括最大索引number_total，Next方法会选择0 - number_total-1之间的数。
                 //获取这个索引位置的值，并将这个索引位置的值与最后一个值交换
                 tempValue = random_index[random_index_choose];
                 random_index[random_index_choose] = random_index[number_total - 1];
                 random_index[number_total - 1] = tempValue;
                 //保存第一个结果选出的值
-                random_value[j - 1] = random_index[number_total - 1];          
+                /*将每次random_index[]的最后一个值保存到最终的结果数列中*/
+                resultShowClassList.Add(new ResultShowClass { ID = j, Result = random_index[number_total - 1] });
                 //将索引值减小，确保最后一个索引只用一次。
-                number_total--;
-
-                //将数据组合好，便于列表中显示
-                resultShowClassList.Add(new ResultShowClass { ID = j, Result = random_value[j - 1] });         
+                number_total--;       
             }
             //展示结果
             lv_resultShow.ItemsSource = resultShowClassList;
